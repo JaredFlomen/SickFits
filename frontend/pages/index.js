3ff4ea7d7977ceafa921eafd79be7c203440c0f5
Jared Flomen <jaredflomen@gmail.com>
2021-03-04 14:31:30 -0500

    Bug Fix

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,9 @@
+import Page from '../components/Page';
+
 export default function IndexPage() {
   return (
-    <div>
-      <p>Index</p>
-    </div>
+    <Page>
+      <p>Hello</p>
+    </Page>
   );
 }
