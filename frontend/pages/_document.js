4dc92f98a1fae2796259b089e2d96886617853d4
Jared Flomen <jaredflomen@gmail.com>
2021-03-04 14:20:29 -0500

    Update _document.js

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,9 +3,8 @@ import Document, { Html, Head, NextScript, Main } from 'next/document';
 export default class MyDocument extends Document {
   render() {
     return (
-      // eslint-disable-next-line prettier/prettier
-      <Html lang='en-CA'>
-        {/* <Head></Head> */}
+      <Html lang="en-CA">
+        <Head />
         <body>
           <Main />
           <NextScript />
