35f4933719e89c838f3ce3c2f39768a3317a0e70
Jared Flomen <jaredflomen@gmail.com>
2021-03-09 12:05:25 -0500

    Update CreateProduct.js

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -24,7 +24,7 @@ export default function CreateProduct() {
       <label htmlFor="price">
         Price
         <input
-          type="text"
+          type="number"
           id="price"
           name="price"
           placeholder="Price"
