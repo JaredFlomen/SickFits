a930bc4e1ac448ee237774f8d1fe727af79b951b
Jared Flomen <jaredflomen@gmail.com>
2021-03-11 14:12:16 -0500

    Update UpdateProduct.js

--- frontend/components/UpdateProduct.js
+++ frontend/components/UpdateProduct.js
@@ -24,7 +24,7 @@ const UPDATE_PRODUCT_MUTATION = gql`
   ) {
     updateProduct(
       id: $id
-      data: { id: $id, name: $name, description: $description, price: $price }
+      data: { name: $name, description: $description, price: $price }
     ) {
       id
       name
@@ -56,7 +56,9 @@ export default function UpdateProduct({ id }) {
         const res = await updateProduct({
           variables: {
             id,
-            data: inputs,
+            name: inputs.name,
+            description: inputs.description,
+            price: inputs.price,
           },
         });
         // Submit the input fields to backend
