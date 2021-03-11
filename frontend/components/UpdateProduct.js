c5dd76e1e04db98609b1fb5a354b55f63c2a9e5a
Jared Flomen <jaredflomen@gmail.com>
2021-03-11 13:50:38 -0500

    Getting the existing product

--- frontend/components/UpdateProduct.js
+++ frontend/components/UpdateProduct.js
@@ -1,3 +1,22 @@
-export default function UpdateProduct() {
-  return <p>Update</p>;
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+
+const SINGLE_PRODUCT_QUERY = gql`
+  query SINGLE_PRODUCT_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+export default function UpdateProduct({ id }) {
+  // Get the existing product
+  const { data, error, loading } = useQuery(SINGLE_PRODUCT_QUERY, {
+    variables: { id },
+  });
+
+  return <p>Update {id}!</p>;
 }
