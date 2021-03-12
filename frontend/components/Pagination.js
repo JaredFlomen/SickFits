6ba5e9b42d30abac0d79b9d0f869cf080f0b9775
Jared Flomen <jaredflomen@gmail.com>
2021-03-12 16:24:00 -0500

    Total page count

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -4,6 +4,7 @@ import gql from 'graphql-tag';
 import { useQuery } from '@apollo/client';
 import PaginationStyles from './styles/PaginationStyles';
 import DisplayError from './ErrorMessage';
+import { perPage } from '../config';
 
 const PAGINATION_QUERY = gql`
   query {
@@ -16,6 +17,7 @@ const PAGINATION_QUERY = gql`
 export default function Pagination({ page }) {
   const { error, loading, data } = useQuery(PAGINATION_QUERY);
   const { count } = data._allProductsMeta;
+  const pageCount = Math.ceil(count / perPage);
   if (loading) return <p>Loading...</p>;
   if (error) return <DisplayError error={error} />;
   return (
@@ -24,7 +26,7 @@ export default function Pagination({ page }) {
         <title>Sick Fits - Page {page} of</title>
       </Head>
       <Link href="/">← Prev</Link>
-      <p>Page x of X </p>
+      <p>Page x of {pageCount} </p>
       <p>{count} Items Total</p>
       <Link href="/">Next →</Link>
     </PaginationStyles>
