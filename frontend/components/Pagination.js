c69df849d6adc9cfd05c95d83cf7747f0c401490
Jared Flomen <jaredflomen@gmail.com>
2021-03-12 16:17:52 -0500

    Base pagination buttons

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import PaginationStyles from './styles/PaginationStyles';
 
 export default function Pagination({ page }) {
@@ -7,6 +8,10 @@ export default function Pagination({ page }) {
       <Head>
         <title>Sick Fits - Page {page} of</title>
       </Head>
+      <Link href="/">← Prev</Link>
+      <p>Page x of X </p>
+      <p>Items Total</p>
+      <Link href="/">Next →</Link>
     </PaginationStyles>
   );
 }
