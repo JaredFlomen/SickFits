8f8be3eeb39a59c7601b39f6bbbd2c9779af1aca
Jared Flomen <jaredflomen@gmail.com>
2021-03-14 15:30:49 -0400

    Sending user info to gql api

--- frontend/components/SignIn.js
+++ frontend/components/SignIn.js
@@ -1,13 +1,37 @@
+import gql from 'graphql-tag';
 import Form from './styles/Form';
 import useForm from '../lib/useForm';
+import { CURRENT_USER_QUERY } from './User';
+
+const SIGNIN_MUTATION = gql`
+  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+    ... on UserAuthenticationWithPasswordSuccess {
+      item {
+        id
+        email
+        name
+      }
+    }
+  }
+`;
 
 export default function SignIn() {
   const { inputs, handleChange, resetForm } = useForm({
     email: '',
     password: '',
   });
+  const [signin, { error, loading }] = useMutation(SIGNIN_MUTATION, {
+    variables: inputs,
+    // Refetch the currently logged in user
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Send the email and password to the graphQL API
+  };
   return (
-    <Form method="post">
+    <Form method="post" onSubmit={handleSubmit}>
+      <h2>Sign Into Your Account</h2>
       <fieldset>
         <label htmlFor="email">
           Email
