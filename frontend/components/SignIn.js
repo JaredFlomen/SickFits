66236806210ceed1a6c57b888488b943af11be33
Jared Flomen <jaredflomen@gmail.com>
2021-03-14 15:16:49 -0400

    Email input

--- frontend/components/SignIn.js
+++ frontend/components/SignIn.js
@@ -2,8 +2,18 @@ import Form from './styles/Form';
 
 export default function SignIn() {
   return (
-    <Form>
-      <p>Test</p>
+    <Form method="post">
+      <fieldset>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            autoComplete="email"
+          />
+        </label>
+      </fieldset>
     </Form>
   );
 }
