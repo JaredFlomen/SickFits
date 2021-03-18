abded95eb4686ecaa0ab5f05ae943258c796566b
Jared Flomen <jaredflomen@gmail.com>
2021-03-18 11:45:36 -0400

    Animating add to cart count

--- frontend/components/CartCount.js
+++ frontend/components/CartCount.js
@@ -12,19 +12,46 @@ const Dot = styled.div`
   font-feature-settings: 'tnum';
   font-variant-numeric: tabular-nums;
 `;
+
+const AnimationStyles = styled.span`
+  position: relative;
+  .count {
+    display: block;
+    position: relative;
+    transition: transform 0.4s;
+    backface-visibility: hidden;
+  }
+  .count-enter {
+    transform: scale(4) rotateX(0.5turn);
+  }
+  .count-enter-active {
+    transform: rotateX(0);
+  }
+  .count-exit {
+    top: 0;
+    position: absolute;
+    transform: rotateX(0);
+  }
+  .count-exit-active {
+    transform: scale(4) rotateX(0.5turn);
+  }
+`;
+
 function CartCount({ count }) {
   return (
-    <TransitionGroup>
-      <CSSTransition
-        unmountOnExit
-        className="count"
-        classNames="count"
-        key={count}
-        timeout={{ enter: 400, exit: 400 }}
-      >
-        <Dot>{count}</Dot>;
-      </CSSTransition>
-    </TransitionGroup>
+    <AnimationStyles>
+      <TransitionGroup>
+        <CSSTransition
+          unmountOnExit
+          className="count"
+          classNames="count"
+          key={count}
+          timeout={{ enter: 400, exit: 400 }}
+        >
+          <Dot>{count}</Dot>;
+        </CSSTransition>
+      </TransitionGroup>
+    </AnimationStyles>
   );
 }
 
