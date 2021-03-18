3ef5ae23f49471424a791ace692fd6d7301bd31a
Jared Flomen <jaredflomen@gmail.com>
2021-03-18 11:35:28 -0400

    Cart Count

---
+++ frontend/components/CartCount.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Dot = styled.div`
+  background: var(--red);
+  color: white;
+  border-radius: 50%;
+  padding: 0.5rem;
+  line-height: 2rem;
+  min-width: 3rem;
+  margin-left: 1rem;
+`;
+function CartCount({ count }) {
+  return <Dot>{count}</Dot>;
+}
+
+export default CartCount;
