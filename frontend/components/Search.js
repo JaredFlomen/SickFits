79a3e53339a4ac2e3e42e36516b39fb12162c512
Jared Flomen <jaredflomen@gmail.com>
2021-03-19 13:06:02 -0400

    spread props to allocate function

--- frontend/components/Search.js
+++ frontend/components/Search.js
@@ -1,20 +1,28 @@
-import { useCombobox } from 'downshift';
+import { resetIdCounter, useCombobox } from 'downshift';
 import { DropDown, DropDownItem, SearchStyles } from './styles/DropDown';
 
 function Search() {
-  const { getMenuProps, getInputProps, getCombobox } = useCombobox({
+  resetIdCounter();
+  const { getMenuProps, getInputProps, getComboboxProps } = useCombobox({
     items: [],
     // Fire when someone type into the box
     onInputValueChange() {},
-    // Fire when someone select from the box
+    // Fire when someone select from the dropdown
     onSelectedItemChange() {},
   });
   return (
     <SearchStyles>
-      <div>
-        <input type='Search' />
+      <div {...getComboboxProps()}>
+        <input
+          {...getInputProps({
+            type: 'search',
+            placeholder: 'Search for an item',
+            id: 'search',
+            className: 'loading',
+          })}
+        />
       </div>
-      <DropDown>
+      <DropDown {...getMenuProps()}>
         <DropDownItem>Hey</DropDownItem>
       </DropDown>
     </SearchStyles>
