4f52c2ff0a85c5ba83e9de5f446334d1f5a319f4
Jared Flomen <jaredflomen@gmail.com>
2021-03-23 14:34:28 -0400

    Ensuring not account for when deleted

--- backend/mutations/checkout.ts
+++ backend/mutations/checkout.ts
@@ -84,7 +84,7 @@ async function checkout(
     }
   });
   //Clean up any old cart item
-  const cartItemIds = cartItems.map(cartItem => cartItem.id);
+  const cartItemIds = user.cart.map(cartItem => cartItem.id);
   await context.lists.CartItem.deleteMany({
     ids: cartItemIds
   });
