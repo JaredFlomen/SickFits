334be37ef1cc86c9d77258a9ee739f0f18a95434
Jared Flomen <jaredflomen@gmail.com>
2021-03-23 14:51:38 -0400

    Displaying error and loading state

--- frontend/pages/order/[id].js
+++ frontend/pages/order/[id].js
@@ -1,5 +1,6 @@
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/client';
+import ErrorMessage from '../../components/ErrorMessage';
 
 const SINGLE_ORDER_QUERY = gql`
   query SINGLE_ORDER_QUERY($id: ID!) {
@@ -30,5 +31,7 @@ export default function SingleOrderPage({ query }) {
   const { data, error, loading } = useQuery(SINGLE_ORDER_QUERY, {
     variables: { id: query.id },
   });
+  if (loading) return <p>Loading</p>;
+  if (error) return <ErrorMessage error={error} />;
   return <div>Hey</div>;
 }
