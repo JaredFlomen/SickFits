5e34aa0d62fd7f6480b5e871391159c0904033e1
Jared Flomen <jaredflomen@gmail.com>
2021-03-23 14:49:29 -0400

    SINGLE_ORDER_QUERY

---
+++ frontend/pages/order/[id].js
@@ -0,0 +1,30 @@
+import gql from 'graphql-tag';
+
+const SINGLE_ORDER_QUERY = gql`
+  query SINGLE_ORDER_QUERY($id: ID!) {
+    order: Order(where: { id: $id }) {
+      id
+      charge
+      total
+      user {
+        id
+      }
+      items {
+        id
+        name
+        description
+        price
+        quantity
+        photo {
+          image {
+            publicUrlTransformed
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default function SingleOrderPage({ query }) {
+  return <div>Hey</div>;
+}
