054f54348fc814a8321b10476f08dda1ab572b76
Jared Flomen <jaredflomen@gmail.com>
2021-03-24 11:33:40 -0400

    Using order page for ordres

--- frontend/pages/orders.js
+++ frontend/pages/orders.js
@@ -1,7 +1,39 @@
-export default function OrderPage() {
-  return (
-    <div>
-      <p>Orders</p>
-    </div>
-  );
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+import Head from 'next/head';
+import ErrorMessage from '../components/ErrorMessage';
+import OrderStyles from '../components/styles/OrderStyles';
+import formatMoney from '../lib/formatMoney';
+
+const USER_ORDERS_QUERY = gql`
+  query USER_ORDERS_QUERY($id: ID!) {
+    allOrders {
+      id
+      charge
+      total
+      user {
+        id
+      }
+      items {
+        id
+        name
+        description
+        price
+        quantity
+        photo {
+          image {
+            publicUrlTransformed
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default function OrdersPage() {
+  const { data, error, loading } = useQuery(USER_ORDERS_QUERY);
+  if (loading) return <p>Loading</p>;
+  if (error) return <ErrorMessage error={error} />;
+  const { allOrders } = data;
+  return ();
 }
