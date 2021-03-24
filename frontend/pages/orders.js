26fa4032f0afa9f903a01616474276708fa88e4c
Jared Flomen <jaredflomen@gmail.com>
2021-03-24 11:35:32 -0400

    Custom styled component

--- frontend/pages/orders.js
+++ frontend/pages/orders.js
@@ -1,6 +1,7 @@
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/client';
 import Head from 'next/head';
+import styled from 'styled-components';
 import ErrorMessage from '../components/ErrorMessage';
 import OrderStyles from '../components/styles/OrderStyles';
 import formatMoney from '../lib/formatMoney';
@@ -30,10 +31,20 @@ const USER_ORDERS_QUERY = gql`
   }
 `;
 
+const OrderUl = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-gap: 4rem;
+`;
+
 export default function OrdersPage() {
   const { data, error, loading } = useQuery(USER_ORDERS_QUERY);
   if (loading) return <p>Loading</p>;
   if (error) return <ErrorMessage error={error} />;
   const { allOrders } = data;
-  return <div>{allOrders.length}</div>;
+  return (
+    <div>
+      <h2>You have {allOrders.length} orders!</h2>
+    </div>
+  );
 }
