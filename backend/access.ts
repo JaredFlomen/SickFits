f434548394434543ebcbe5ca90ec414f0b8d0177
Jared Flomen <jaredflomen@gmail.com>
2021-03-25 12:43:57 -0400

    Function to check if canManageProducts

--- backend/access.ts
+++ backend/access.ts
@@ -6,3 +6,9 @@ export function isSignedIn({ session }: ListAccessArgs) {
   //If session is undefined (double !!) will return false (not signed in)
   return !!session;
 }
+
+export const permissions = {
+  canManageProducts({ session }) {
+    return session.data.role.canManageProducts;
+  },
+}
