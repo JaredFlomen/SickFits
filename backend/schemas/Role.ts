0f219fbc52b9abcf44a9201aaa0d96eb0c80966a
Jared Flomen <jaredflomen@gmail.com>
2021-03-26 11:40:42 -0400

    Importing access to the roles schema

--- backend/schemas/Role.ts
+++ backend/schemas/Role.ts
@@ -1,8 +1,15 @@
 import { relationship, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
+import { permissions } from '../access';
 import { permissionFields } from './fields';
 
 export const Role = list({
+  access: {
+    create: permissions.canManageRoles,
+    read: permissions.canManageRoles,
+    update: permissions.canManageRoles,
+    delete: permissions.canManageRoles,
+  },
   fields: {
     name: text({ isRequired: true }),
     ...permissionFields,
